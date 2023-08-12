e92c2049df446ecc00d60b51cc85b0da7239ef5b
Roman Kutsenko <73640616+KutsenkoRN@users.noreply.github.com>
2023-08-13 00:49:21 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H_W_Mod3_3
+{
+    internal class Program
+    {
+       public static void Main(string[] args)
+        {
+            Class1 cl1 = new Class1();
+            cl1.ShowHandler = Show;
+            cl1.ShowHandler(new Class2().ResultDelegate(3));
+        }
+
+        public static void Show(bool result)
+        {
+            Console.WriteLine(result);
+        }
+    }
+}
